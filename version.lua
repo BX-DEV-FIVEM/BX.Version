232cd042a6076e1cc857416eb4db7d09f3db32ba
BX-DEV <140925178+BX-DEV-FIVEM@users.noreply.github.com>
2024-10-26 13:54:53 +0200

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,6 +1,6 @@
 version = {}
 version["BX-Intro"] = 'V3.2'
-version["BX-CustomJob"] = 'V1.5.2'
+version["BX-CustomJob"] = 'V1.5.3'
 version["BX-GoFast"] = 'V1.0'
 version["BX-CarJob"] = 'V1.0'
 version["BX-NpcRob"] = 'V2.0'
